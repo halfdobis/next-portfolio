de6153a052474907ad267f1797111af1849d53d6
missdobis <missdobis@gmail.com>
2023-09-06 04:21:07 +0100

    Page-Transition with Framer-motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
-    <Component {...pageProps} />
-  </Layout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
   );
 }
 
