37fcc17e723cd665a140e582c7641e267601fe3f
missdobis <missdobis@gmail.com>
2023-09-06 07:29:48 +0100

    HomePage-[Hero-Section-{Project-Animated-Button}]

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,38 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          <h1 className="h1">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:pb-40 xl:text-left h-full container mx-auto">
+          <motion.h1
+          variants={fadeIn("down", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h1">
             Transforming ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl-mb-16">
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl-mb-16"
+          >
             Master Full Stack E-Commerce development with Next.js 13, React,
             Tailwind, Prisma, and MySQL, including Dashboard and CMS creation.
-          </p>
+          </motion.p>
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       <div>image</div>
