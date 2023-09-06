2fd893b7011dca3d3daa0f52a1c5d5f419474494
missdobis <missdobis@gmail.com>
2023-09-06 05:03:21 +0100

    HomePage-[Hero-Section-Incomplete]

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import Image from "next/image";
+
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <h1 className="h1">
+            Transforming ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl-mb-16">
+            Master Full Stack E-Commerce development with Next.js 13, React,
+            Tailwind, Prisma, and MySQL, including Dashboard and CMS creation.
+          </p>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+        </div>
+      </div>
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
