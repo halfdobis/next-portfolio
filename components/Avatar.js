9b34aefec1e638742b267ee00e2e7bee63c652db
missdobis <missdobis@gmail.com>
2023-09-06 18:38:39 +0100

    HomePage-[Particles-Container and Animation]

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
-      <Image 
+      <Image
       src={'/avatar.png'}
       width={737}
       height={678}
