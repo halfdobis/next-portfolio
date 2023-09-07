92a9a50b133af09368394a31daf244ffef504720
missdobis <missdobis@gmail.com>
2023-09-07 05:11:10 +0100

    AboutPage

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,9 +5,9 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-      src={'/avatar.png'}
-      width={636}
-      height={557}
+      src={'/avatar1.png'}
+      width={500}
+      height={400}
       alt="avatar"
       className="translate-z-0 w-full h-full"
       />
